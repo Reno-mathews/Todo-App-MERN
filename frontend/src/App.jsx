435e48cfb8e4431b0a62f8d4d68c1983cade2af8
Reno-mathews <renomathews10pro@gmail.com>
2026-01-10 08:38:48 +0530

    Update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import ToDoForm from "./components/ToDoForm";
+import ToDoList from "./components/ToDoList";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+
+const fetchTodos = async () => {
+  const res = await fetch("http://localhost:5000/api/todos");
+  const data = await res.json();
+  setTodos(data);
+};
+
+const addTodo = async (title) => {
+  const res = await fetch()
+}
+}
