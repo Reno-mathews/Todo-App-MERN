4d7927a860aa2789d851986f42e8323421c078f5
Reno-mathews <renomathews10pro@gmail.com>
2026-01-11 07:50:10 +0530

    Update

--- backend/server2.js
+++ backend/server2.js
@@ -12,3 +12,21 @@ app.use(express.json());
 app.use("/api/todos", todoRoutes);
 
 // Test route 
+app.get("/", (req,res) => {
+    res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+//MongoDB connection
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected");
+        app.listen(PORT, () => {
+            console.log(`Server on running on port ${PORT}`)
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    });
