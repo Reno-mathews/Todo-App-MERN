0bae3c09d41d01c86b9a43703210d89d6e26acf2
Reno-mathews <renomathews10pro@gmail.com>
2026-01-11 10:12:54 +0530

    Update

--- backend/todos.js
+++ backend/todos.js
@@ -3,28 +3,28 @@ const Todo = require("../models/Todo");
 
 const router = express.Router();
 
-//GET all todos
-router.get("/", async(req,res) => {
+// GET all todos
+router.get("/", async (req, res) => {
     try {
         const todos = await Todo.find();
         res.json(todos);
     } catch (err) {
-        res.status(500),json( { message: "Error fetching todos" });
+        res.status(500).json( { message: "Error fetching todos"});
     }
 });
 
 // POST a new todo
-router.post("/", async (req,res) => {
-    try{
-        const newTodo = newTodo({
+router.post("/", async (req, res) => {
+    try {
+        const newTodo = new Todo({
             title: req.body.title,
         });
 
         const savedTodo = await newTodo.save();
         res.json(savedTodo);
-    } catch (err) {
-        res.status(500).json( { message: "Error creating todo"});
+    }   catch (err) {
+        res.status(500).json({ message: "Error creating todo"});
     }
 });
 
-module.exports = router;
+module.exports = router; 
