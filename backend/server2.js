9cd4d6b365416351ff82f7296a020d08fedaba16
Reno-mathews <renomathews10pro@gmail.com>
2026-01-11 08:56:07 +0530

    Update

--- backend/server2.js
+++ backend/server2.js
@@ -9,3 +9,25 @@ const app = express();
 // Middleaware
 app.use(cors);
 app.use(express.json());
+app.use("/api/todos", todoRoutes);
+
+// Test route
+app.get("/", (req,res) => {
+    res.send("API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+// MongoDB connection
+
+mongoose 
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected");
+        app.listen(PORT, () => {
+            console.log(`Server on running on port ${PORT}`)
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    });
